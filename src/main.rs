326fca3c36c94835145a686ff24003f066477d60
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2024-08-04 18:43:20 +0900

    Implement Kitasenjyu Chess Club actual scraper

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ fn main() {
     for e in events {
         println!("================ event ==============");
         println!("date: {:?}", e.date);
+        println!("open_time: {:?}", e.open_time);
+        println!("revenue: {:?}", e.revenue);
+        println!("fee: {:?}", e.fee);
     }
 }
 
@@ -118,52 +121,50 @@ pub struct EventInfo {
 struct EventScraperKitasenjyu;
 impl ChessEventScraper for EventScraperKitasenjyu {
     fn scrape_event(&self) -> Vec<EventInfo> {
-        let url = "https://8by8.hatenablog.com/".to_string();
-        // FIXME: tentative mock implementation
-        let mut events: Vec<EventInfo> = Vec::new();
-
-
+        println!("======This is EventScraperKitasenjyu=======");
+        let url = "http://chess.m1.valueserver.jp/".to_string();
+        let charset = "Shift_JIS";  // need to manually set charset
         let body = 
             reqwest::blocking::get(url)
-            .unwrap().text().unwrap();
+            .unwrap().text_with_charset(charset).unwrap();
         let document = scraper::Html::parse_document(&body);
 
         let scrape_target_selector = 
-            scraper::Selector::parse("article.entry-content").unwrap();
+            scraper::Selector::parse("div.item").unwrap();
         let scrape_target_list = document.select(&scrape_target_selector);
-        let paragraph_selector = scraper::Selector::parse("p").unwrap();
 
         let mut events = Vec::new();
         for article in scrape_target_list {
-            // log::info!("================");
+            let text = article.text().collect::<Vec<_>>().join("");
+
+            if !text.contains("公式戦例会予定") {
+                continue;
+            }
+
             let mut date = String::from("");
             let mut open_time = String::from("");
             let mut revenue = String::from("");
             let mut fee = String::from("");
-            for e in article.select(&paragraph_selector) {
-                let text = e.text().collect::<Vec<_>>().join("");
-                // log::info!("text: {:?}", text);
 
-                if text.contains("場所:") {
-                    revenue = String::from(text.trim().trim_start_matches("場所:").trim());
+            // TODO fix this not clean code
+            for line in text.lines() {
+                if !line.contains("日本チェス連盟公式戦２Ｒ") {
+                    continue
                 }
-                if text.contains("日時:") {
-                    date = String::from(text.trim().trim_start_matches("日時:").trim());
-                    date = trim_left(
-                        &date,
-                        Vec::from([String::from("(定員"), String::from("（定員")]),
-                    );
-                }
-                if text.contains("参加費:") {
-                    fee = String::from(text.trim().trim_start_matches("参加費:").trim());
-                }
-                let re = regex::Regex::new(r"(\d{2})時\d{2}分〜\d{2}時\d{2}分").unwrap();
-                if re.is_match(&text) {
-                    open_time = String::from(text.trim());
-                    // log::info!("open_time: {:?}", open_time);
-                } else {
-                    // log::info!("NOT open_time: {:?}", text);
-                }
+
+                let infos: Vec<&str> = line.split("\u{3000}").collect();
+                date = infos[1].to_string();
+                open_time = infos[2].to_string();
+                revenue = infos[3].to_string();
+                fee = "unknown".to_string()
+            }
+
+            // not or not well structured event
+            if date == "" ||
+               open_time == "" ||
+               revenue == "" ||
+               fee == "" {
+                continue;
             }
 
             let e = EventInfo {
